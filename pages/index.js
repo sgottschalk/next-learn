ea596f6c55e50be941fc1add616a98000b00d846
Sean Gottschalk <seangottschalk@gmail.com>
2019-11-25 15:11:04 -0800

    [client+server fetch] Batman example

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,20 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
-}
-
-const PostLink = ({ post }) => (
-    <li>
-      <Link href="/p/[id]" as={`/p/${post.id}`}>
-        <a>{post.title}</a>
-      </Link>
-      <style jsx>{`
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-  
-        a {
-          text-decoration: none;
-          color: blue;
-          font-family: 'Arial';
-        }
-  
-        a:hover {
-          opacity: 0.6;
-        }
-      `}</style>
-    </li>
-  );
-
-export default function Blog() {
-  return (
+const Index = props => (
     <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        {getPosts().map(post => (
-          <PostLink key={post.id} post={post} />
-        ))}
-      </ul>
-      <style jsx>{`
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <li key={show.id}>
+                    <Link href='/p/[id]' as={`/p/${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        <style jsx>{`
         h1,
         a {
           font-family: 'Arial';
@@ -67,5 +39,17 @@ export default function Blog() {
         }
       `}</style>
     </Layout>
-  );
-}
+);
+
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count = ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+};
+
+export default Index;
